9f977b269f153a487db91b45574287e647038b38
BrandonTang219 <71780289+BrandonTang219@users.noreply.github.com>
2021-03-26 01:48:43 -0400

    slowly starting
    got a question mapped out, finally inserted npm start commands

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+[
+  {
+    questionContent: "What kind of energy do Sorcerors in Jujutsu Kaisen?",
+    choices: [
+      { choice: "Spiritual", value: 0 },
+      { choice: "Cursed", value: 1 },
+      { choice: "Condensed", value: 0 },
+      { choice: "Soul", value: 0 },
+    ],
+  },
+];
