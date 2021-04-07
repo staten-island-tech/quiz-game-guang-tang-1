39516df6cd274f6b8f78238c604900559e59b4bc
Brandon Tang <71780289+BrandonTang219@users.noreply.github.com>
2021-04-07 14:04:44 -0400

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ questions created, js next
    - added a handful of more questions
    - changed package.json to have dist/index.html instead of index.js
    - added font to header

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+const {
+
+};
+
 [
   {
     questionContent: "What kind of energy do Sorcerors in Jujutsu Kaisen?",
@@ -6,6 +10,69 @@
       { choice: "Cursed", value: 1 },
       { choice: "Condensed", value: 0 },
       { choice: "Soul", value: 0 },
+    ],
+  },
+  {
+    questionContent: "What is the type of animal hoodie that Umaru-chan wears?",
+    choices: [
+      { choice: "Bear", value: 0 },
+      { choice: "Cat", value: 0 },
+      { choice: "Raccoon", value: 0 },
+      { choice: "Hamster", value: 1 },
+    ],
+  },
+  {
+    questionContent: "Which of these characters are not a part of the Big Three?",
+    choices: [
+      { choice: "Goku", value: 1 },
+      { choice: "Naruto", value: 0 },
+      { choice: "Ichigo", value: 0 },
+      { choice: "Luffy", value: 0 },
+    ],
+  },
+  {
+    questionContent: "Which of these games is from a Japanese company?",
+    choices: [
+      { choice: "Genshin Impact", value: 0 },
+      { choice: "Granblue Fantasy", value: 1 },
+      { choice: "Arknights", value: 0 },
+      { choice: "Azur Lane", value: 0 },
+    ],
+  },
+  {
+    questionContent: "Which of these servants is a Saber class?",
+    choices: [
+      { choice: "Jeanne d'Arc", value: 0 },
+      { choice: "Gilgamesh", value: 0 },
+      { choice: "Mordred", value: 1 },
+      { choice: "Astolfo", value: 0 },
+    ],
+  },
+  {
+    questionContent: "Which of these films was not directed by Hayao Miyazaki?",
+    choices: [
+      { choice: "My Neighbor Totoro", value: 0 },
+      { choice: "Grave of the Fireflies", value: 1 },
+      { choice: "Howl's Moving Castle", value: 0 },
+      { choice: "Princess Mononoke", value: 0 },
+    ],
+  },
+  {
+    questionContent: "Which of these is studios was formed specifically for Mushoku Tensei?",
+    choices: [
+      { choice: "MAPPA", value: 0 },
+      { choice: "Studio Bind", value: 0 },
+      { choice: "Wit Studio", value: 1 },
+      { choice: "A-1 Pictures", value: 0 },
+    ],
+  },
+  {
+    questionContent: "Which of these movies grossed the most at the box office?",
+    choices: [
+      { choice: "A Silent Voice", value: 0 },
+      { choice: "Weathering With You", value: 0 },
+      { choice: "Your Name", value: 0 },
+      { choice: "Spirited Away", value: 1 },
     ],
   },
 ];
