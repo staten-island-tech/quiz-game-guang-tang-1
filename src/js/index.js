7b06bcda9abc6d04f0d5ec54ff4013b4a1eab37f
Brandon Tang <71780289+BrandonTang219@users.noreply.github.com>
2021-04-11 19:15:17 -0400

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ soon tm (innerhtml on next)
    need to figure out how to access arrays and apply the choices and questions to respective areas in HTML after clicking next.

--- src/js/index.js
+++ src/js/index.js
@@ -1,82 +1,3 @@
-/* [
-  {
-    questionContent: "What kind of energy do Sorcerors in Jujutsu Kaisen?",
-    choices: [
-      { choice: "Spiritual", value: 0 },
-      { choice: "Cursed", value: 1 },
-      { choice: "Condensed", value: 0 },
-      { choice: "Soul", value: 0 },
-    ],
-  },
-  {
-    questionContent: "What is the type of animal hoodie that Umaru-chan wears?",
-    choices: [
-      { choice: "Bear", value: 0 },
-      { choice: "Cat", value: 0 },
-      { choice: "Raccoon", value: 0 },
-      { choice: "Hamster", value: 1 },
-    ],
-  },
-  {
-    questionContent:
-      "Which of these characters are not a part of the Big Three?",
-    choices: [
-      { choice: "Goku", value: 1 },
-      { choice: "Naruto", value: 0 },
-      { choice: "Ichigo", value: 0 },
-      { choice: "Luffy", value: 0 },
-    ],
-  },
-  {
-    questionContent: "Which of these games is from a Japanese company?",
-    choices: [
-      { choice: "Genshin Impact", value: 0 },
-      { choice: "Granblue Fantasy", value: 1 },
-      { choice: "Arknights", value: 0 },
-      { choice: "Azur Lane", value: 0 },
-    ],
-  },
-  {
-    questionContent: "Which of these servants is a Saber class?",
-    choices: [
-      { choice: "Jeanne d'Arc", value: 0 },
-      { choice: "Gilgamesh", value: 0 },
-      { choice: "Mordred", value: 1 },
-      { choice: "Astolfo", value: 0 },
-    ],
-  },
-  {
-    questionContent: "Which of these films was not directed by Hayao Miyazaki?",
-    choices: [
-      { choice: "My Neighbor Totoro", value: 0 },
-      { choice: "Grave of the Fireflies", value: 1 },
-      { choice: "Howl's Moving Castle", value: 0 },
-      { choice: "Princess Mononoke", value: 0 },
-    ],
-  },
-  {
-    questionContent:
-      "Which of these is studios was formed specifically for Mushoku Tensei?",
-    choices: [
-      { choice: "MAPPA", value: 0 },
-      { choice: "Studio Bind", value: 0 },
-      { choice: "Wit Studio", value: 1 },
-      { choice: "A-1 Pictures", value: 0 },
-    ],
-  },
-  {
-    questionContent:
-      "Which of these movies grossed the most at the box office?",
-    choices: [
-      { choice: "A Silent Voice", value: 0 },
-      { choice: "Weathering With You", value: 0 },
-      { choice: "Your Name", value: 0 },
-      { choice: "Spirited Away", value: 1 },
-    ],
-  },
-];
-*/
-
 import { DOMSelectors} from "./dom";
 import {questions} from "./questions"
 
@@ -96,17 +17,29 @@ DOMSelectors.choiceFour.addEventListener("click", answerFour)
 function answerFour() {
   console.log("answer 4")
 }
-/* DOMSelectors.start.addEventListener('click', startButton)
+DOMSelectors.start.addEventListener('click', startButton)
 function startButton() {
-  console.log("clicked the start button")
+  console.log("clicked the start button!")
+}
+DOMSelectors.back.addEventListener('click', backButton)
+function backButton() {
+  console.log("clicked the back button!")
 }
 DOMSelectors.next.addEventListener('click', nextButton)
 function nextButton() {
-  console.log("clicked the next button")
-} */
+  console.log("clicked the next button!")
+} 
 
 // next to complete
 // Get questions and answers working and substituted into the HTML properly
 // Get the page to be able to recognize a correct answer, incorrect one.
 // Get the page to change questions and answers if a correct answer is clicked.
 
+console.log(questions);
+
+function nextButton() {
+  DOMSelectors.choiceOne.innerHTML = "working"
+  DOMSelectors.choiceTwo.innerHTML = "working"
+  DOMSelectors.choiceThree.innerHTML = "working"
+  DOMSelectors.choiceFour.innerHTML = "working"
+}
--- src/js/questions.js
+++ src/js/questions.js
@@ -2,84 +2,67 @@ const questions = [
 
   {
     questionContent: "What kind of energy do Sorcerors in Jujutsu Kaisen?",
-    choices: [
-      { choiceOne: "Spiritual" },
-      { choiceTwo: "Cursed" },
-      { choiceThree: "Condensed" },
-      { choiceFour: "Soul" },
-    ],
-    correctAnswer: "choiceTwo"
+      choice1: "Spiritual",
+      choice2: "Cursed",
+      choice3: "Condensed",
+      choice4: "Soul",
+      answer: 2,
   },
   {
     questionContent: "What is the type of animal hoodie that Umaru-chan wears?",
-    choices: [
-      { choiceOne: "Bear" },
-      { choiceTwo: "Cat" },
-      { choiceThree: "Raccoon" },
-      { choiceFour: "Hamster" },
-    ],
-    correctAnswer: "choiceFour"
+      choice1: "Bear",
+      choice2: "Cat",
+      choice3: "Raccoon",
+      choice4: "Hamster",
+      answer: 4,
   },
   {
     questionContent: "Which of these characters are not a part of the Big Three?",
-    choices: [
-      { choiceOne: "Goku" },
-      { choiceTwo: "Naruto" },
-      { choiceThree: "Ichigo" },
-      { choiceFour: "Luffy" },
-    ],
-    correctAnswer: "choiceOne"
-
+      choice1: "Goku",
+      choice2: "Naruto",
+      choice3: "Ichigo",
+      choice4: "Luffy",
+      answer: 1,
   },
   {
     questionContent: "Which of these games is from a Japanese company?",
-    choices: [
-      { choiceOne: "Genshin Impact" },
-      { choiceTwo: "Granblue Fantasy" },
-      { choiceThree: "Arknights" },
-      { choiceFour: "Azur Lane" },
-    ],
-    correctAnswer: "choiceTwo"
+      choice1: "Genshin Impact",
+      choice2: "Granblue Fantasy",
+      choice3: "Arknights",
+      choice4: "Azur Lane",
+      answer: 2,
   },
   {
     questionContent: "Which of these servants is a Saber class?",
-    choices: [
-      { choiceOne: "Jeanne d'Arc" },
-      { choiceTwo: "Gilgamesh" },
-      { choiceThree: "Mordred" },
-      { choiceFour: "Astolfo" },
-    ],
-    correctAnswer: "choiceThree"
+      choice1: "Jeanne d'Arc",
+      choice2: "Gilgamesh",
+      choice3: "Mordred",
+      choice4: "Astolfo",
+      answer: 3,
   },
   {
     questionContent: "Which of these films was not directed by Hayao Miyazaki?",
-    choices: [
-      { choiceOne: "My Neighbor Totoro" },
-      { choiceTwo: "Grave of the Fireflies" },
-      { choiceThree: "Howl's Moving Castle" },
-      { choiceFour: "Princess Mononoke" },
-    ],
-    correctAnswer: "choiceTwo"
+      choice1: "My Neighbor Totoro",
+      choice2: "Grave of the Fireflies",
+      choice3: "Howl's Moving Castle",
+      choice4: "Princess Mononoke",
+      answer: 2,
   },
   {
     questionContent: "Which of these is studios was formed specifically for Mushoku Tensei?",
-    choices: [
-      { choiceOne: "MAPPA" },
-      { choiceTwo: "Studio Bind" },
-      { choiceThree: "Wit Studio" },
-      { choiceFour: "A-1 Pictures" },
-    ],
-    correctAnswer: "choiceTwo"
+      choice1: "MAPPA",
+      choice2: "Studio Bind",
+      choice3: "Wit Studio",
+      choice4: "A-1 Pictures",
+      answer: 2,
   },
   {
     questionContent: "Which of these movies grossed the most at the box office?",
-    choices: [
-      { choiceOne: "A Silent Voice" },
-      { choiceTwo: "Weathering With You" },
-      { choiceThree: "Your Name" },
-      { choiceFour: "Spirited Away" },
-    ],
-    correctAnswer: "choiceFour"
+      choice1: "A Silent Voice",
+      choice2: "Weathering With You",
+      choice3: "Your Name",
+      choice4: "Spirited Away",
+      answer: 1,
   },
 
 ]
