664e87327aa6211df72a301af61bdad9b8592af9
Brandon Tang <71780289+BrandonTang219@users.noreply.github.com>
2021-04-15 14:09:59 -0400

    (◕﹏◕✿) fixing netlify issues
    removed /dist from package.json

--- src/js/high.js
+++ src/js/high.js
@@ -1,14 +1,7 @@
-const DOMSelectors = {
-    question: document.querySelector('#questions-text'),
-    choiceOne: document.querySelector('#answer-choices button:nth-child(1)'),
-    choiceTwo: document.querySelector('#answer-choices button:nth-child(2)'),
-    choiceThree: document.querySelector('#answer-choices button:nth-child(3)'),
-    choiceFour: document.querySelector('#answer-choices button:nth-child(4)'),
-    start: document.querySelector('#control button:nth-child(1)'),
-    back: document.querySelector('#control button:nth-child(2)'),
-    next: document.querySelector('#control button:nth-child(3)'),
+const highScoresList = document.querySelector('#highScoresList')
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
-    question: document.getElementById('questions'),
-  };
-  
-  export { DOMSelectors };
+highScoresList.innerHTML =
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join("")
